c8abcf490a3decb40a70107b05c139524cf0d312
gittinjiggywithit <kasperfriis93@gmail.com>
2017-05-06 18:26:29 +0200

    Merge branch 'master' into kasper
    # Conflicts:
    #	app/src/main/java/com/example/emilstepanian/justhandworker/jobtaker/ui/JobTakerMainActivity.java
    #	app/src/main/java/com/example/emilstepanian/justhandworker/jobtaker/ui/ProfileContainerFragment.java

--- app/src/main/java/com/example/emilstepanian/justhandworker/shared/ui/MessagesTabFragment.java
+++ app/src/main/java/com/example/emilstepanian/justhandworker/shared/ui/MessagesTabFragment.java
@@ -1,7 +1,5 @@
-package com.example.emilstepanian.justhandworker.workman.ui;
+package com.example.emilstepanian.justhandworker.shared.ui;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
